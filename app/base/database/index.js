a3cb08e54550b224abb534a951081000f057f93a
MaximilliaanJA-2467787 <maximiliaan.janssen@student.uhasselt.be>
2025-10-17 15:30:16 +0200

    add initial start

---
+++ app/base/database/index.js
@@ -0,0 +1,24 @@
+const SqliteDatabase = require('better-sqlite3');
+const config = require('../../config');
+let sqlite = null;
+
+
+class Database {
+    constructor() {
+        sqlite = new SqliteDatabase("database.db", { verbose: console.log });
+
+        sqlite.pragma("journal_mode = WAL;");
+        sqlite.pragma("busy_timeout = 5000;");
+        sqlite.pragma("synchronous = NORMAL;");
+        sqlite.pragma("cache_size = 1000000000;");
+        sqlite.pragma("foreign_keys = true;");
+        sqlite.pragma("temp_store = memory;");
+    }
+
+}
+
+const database = new Database();
+module.exports = {
+    sqlite: sqlite, 
+    database: database
+}
