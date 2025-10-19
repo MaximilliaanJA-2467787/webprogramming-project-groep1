71e7172f92d7e15848c49200f0c45412276f4e0e
MaximilliaanJA-2467787 <maximiliaan.janssen@student.uhasselt.be>
2025-10-19 17:25:26 +0200

    Toevoegen tablebuilder

--- app/base/database/index.js
+++ app/base/database/index.js
@@ -2,23 +2,21 @@ const SqliteDatabase = require('better-sqlite3');
 const config = require('../../config');
 let sqlite = null;
 
-
 class Database {
     constructor() {
-        sqlite = new SqliteDatabase("database.db", { verbose: console.log });
+        sqlite = new SqliteDatabase('database.db', { verbose: console.log });
 
-        sqlite.pragma("journal_mode = WAL;");
-        sqlite.pragma("busy_timeout = 5000;");
-        sqlite.pragma("synchronous = NORMAL;");
-        sqlite.pragma("cache_size = 1000000000;");
-        sqlite.pragma("foreign_keys = true;");
-        sqlite.pragma("temp_store = memory;");
+        sqlite.pragma('journal_mode = WAL;');
+        sqlite.pragma('busy_timeout = 5000;');
+        sqlite.pragma('synchronous = NORMAL;');
+        sqlite.pragma('cache_size = 1000000000;');
+        sqlite.pragma('foreign_keys = true;');
+        sqlite.pragma('temp_store = memory;');
     }
-
 }
 
 const database = new Database();
 module.exports = {
-    sqlite: sqlite, 
-    database: database
-}
+    sqlite: sqlite,
+    database: database,
+};
